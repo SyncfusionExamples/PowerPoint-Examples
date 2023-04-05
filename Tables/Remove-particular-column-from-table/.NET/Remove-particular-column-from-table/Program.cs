ee0cde8aa1d7c7bbb07498835f9a3f37283f2769
AzureAD\AntoNihilSahayaRaj <antonihil.sahayaraj@syncfusion.com>
2023-04-05 18:43:06 +0530

    Added samples of the deleting rowand column from table

---
+++ Tables/Remove-particular-column-from-table/.NET/Remove-particular-column-from-table/Program.cs
@@ -0,0 +1,14 @@
+﻿using Syncfusion.Presentation;
+
+//Load or open an PowerPoint Presentation.
+using FileStream inputStream = new(Path.GetFullPath(@"../../../Data/Template.pptx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+//Open an existing PowerPoint presentation.
+using IPresentation pptxDoc = Presentation.Open(inputStream);
+//Get slide from the Presentation.
+ISlide slide = pptxDoc.Slides[0];
+//Get the table from slide.
+ITable table = slide.Shapes[0] as ITable;
+//Remove the column from the table.
+table.Columns.RemoveAt(4);
+using FileStream outputStream = new(Path.GetFullPath(@"../../../Result.pptx"), FileMode.Create, FileAccess.ReadWrite);
+pptxDoc.Save(outputStream);
