b147010c1d6507551ae9a92a5ce2430b86d63756
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-06-23 12:30:30 +0530

    sample Added

---
+++ Find-and-Replace/Find-and-Replace-in-Powerpoint-using-Regex/.NET/Find-and-Replace-in-PowerPoint-using-regex/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Presentation;
+using System.Text.RegularExpressions;
+
+
+
+//Load or open a PowerPoint Presentation.
+using FileStream inputStream = new(Path.GetFullPath(@"../../../Data/Input.pptx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+//Open an existing PowerPoint presentation.
+using IPresentation pptxDoc = Presentation.Open(inputStream);
+
+//Find all the occurrences of a given pattern of text in the PowerPoint presentation using Regex.
+ITextSelection[] textSelections = pptxDoc.FindAll(new Regex("{[A-Za-z]+}"));
+foreach (ITextSelection textSelection in textSelections)
+{
+    //Get the found text as a single text part.
+    ITextPart textPart = textSelection.GetAsOneTextPart();
+    //Replace the text.
+    textPart.Text = "Service";
+}
+//Saves the Presentation.
+using FileStream outputStream = new(Path.GetFullPath(@"../../../Output.pptx"), FileMode.Create, FileAccess.ReadWrite);
+pptxDoc.Save(outputStream);
+
+
