a81a4bae88921511004d4b8327731d1d0a291f11
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-06-26 18:47:22 +0530

    Mac sample added

---
+++ Read-and-save-PowerPoint-presentation/Open-and-save-PowerPoint/Mac/Open_and_Save_PowerPoint_Presentation/Program.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Presentation;
+
+namespace Open_and_Save_PowerPoint_Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open an existing PowerPoint presentation
+            IPresentation pptxDoc = Presentation.Open(new FileStream(Path.GetFullPath(@"../../../Data/Input.pptx"), FileMode.Open));
+            //Gets the first slide from the PowerPoint presentation
+            ISlide slide = pptxDoc.Slides[0];
+            //Gets the first shape of the slide
+            IShape shape = slide.Shapes[0] as IShape;
+            //Change the text of the shape
+            if (shape.TextBody.Text == "Company History")
+                shape.TextBody.Text = "Company Profile";
+            //Save the PowerPoint presentation as stream
+            FileStream outputStream = new FileStream("Output.pptx", FileMode.Create);
+            pptxDoc.Save(outputStream);
+            outputStream.Position = 0;
+            outputStream.Flush();
+            outputStream.Dispose();
+            //Close the PowerPoint presentation
+            pptxDoc.Close();
+        }
+    }
+}
+
+           
