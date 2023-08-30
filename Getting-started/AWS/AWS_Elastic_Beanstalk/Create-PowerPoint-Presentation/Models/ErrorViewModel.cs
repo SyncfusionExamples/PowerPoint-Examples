8b6497ed09df0240b8c33404a454e35ce565dd36
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-08-30 15:04:10 +0530

    Sample Added

---
+++ Getting-started/AWS/AWS_Elastic_Beanstalk/Create-PowerPoint-Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Create_PowerPoint_Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
