8f82935a8644f5913109a818107dd6f59056d3f2
DharanitharanA <97955635+DharanitharanA@users.noreply.github.com>
2023-09-12 19:07:34 +0530

    Removed the PPTX to Image samples

--- PPTX-to-Image-conversion/Convert-PowerPoint-presentation-to-Image/GCP/Google_App_Engine/Convert-PPTX-to-image/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Convert_PPTX_to_image.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
