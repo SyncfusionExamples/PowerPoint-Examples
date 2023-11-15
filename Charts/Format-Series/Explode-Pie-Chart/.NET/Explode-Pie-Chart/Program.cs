b877c3291c43db79184bb40e05eae7f7ded73df4
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-11-15 14:24:43 +0530

    Sample added

---
+++ Charts/Format-Series/Explode-Pie-Chart/.NET/Explode-Pie-Chart/Program.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.OfficeChart;
+using Syncfusion.Presentation;
+
+namespace Explode_Pie_Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/Template.pptx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            //Open an existing PowerPoint Presentation.
+            using (IPresentation pptxDoc = Presentation.Open(fileStreamPath))
+            {
+                //Gets the first slide.
+                ISlide slide = pptxDoc.Slides[0];
+                //Gets the chart in slide.
+                IPresentationChart chart = slide.Shapes[0] as IPresentationChart;
+
+                //Exploding the pie chart to 40%.
+                chart.Series[0].SerieFormat.Percent = 40;
+
+                //Sets position of legend.
+                chart.Legend.Position = OfficeLegendPosition.Bottom;
+
+                using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.pptx"), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    //Save the PowerPoint Presentation.
+                    pptxDoc.Save(outputStream);
+                }
+            }
+        }
+    }
+}
