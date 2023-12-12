56368dc475d97a5a84e5309b47db42734d92f9ce
Lokesh Baskar <lokesh.baskar@syncfusion.com>
2023-12-12 13:41:55 +0530

    Added or modified the fallback font code snippet in the UG

---
+++ PPTX-to-Image-conversion/Fallback-fonts-based-on-scripttype/.NET/Fallback-fonts-based-on-scripttype/Program.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.Office;
+using Syncfusion.Presentation;
+using Syncfusion.Office;
+namespace Fallback_fonts_based_on_scripttype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the PowerPoint presentation into stream
+            using FileStream fileStreamInput = new FileStream(@"../../../Data/Template.pptx", FileMode.Open, FileAccess.Read);
+            //Open the existing PowerPoint presentation with loaded stream
+            using IPresentation pptxDoc = Presentation.Open(fileStreamInput);
+            //Adds fallback font for "Arabic" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Arabic, "Arial, Times New Roman, Courier New, Segoe UI, Traditional Arabic");
+            //Adds fallback font for "Hebrew" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Hebrew, "Arial, Times New Roman, David, Courier New, Microsoft Sans Serif");
+            //Adds fallback font for "Hindi" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Hindi, "Mangal, Nirmala UI, Utsaah");
+            //Adds fallback font for "Chinese" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Chinese, "DengXian, MingLiU, MS Gothic, SimSun");
+            //Adds fallback font for "Japanese" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Japanese, "Yu Mincho, MS Mincho, MS Gothic");
+            //Adds fallback font for "Thai" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Thai, "Tahoma, Microsoft Sans Serif");
+            //Adds fallback font for "Korean" script type
+            pptxDoc.FontSettings.FallbackFonts.Add(ScriptType.Korean, "Malgun Gothic, Batang, Dotum, Gulim");
+            //Convert PowerPoint slide to image as stream
+            using Stream stream = pptxDoc.Slides[0].ConvertToImage(ExportImageFormat.Jpeg);
+            //Reset the stream position
+            stream.Position = 0;
+            //Create the output image file stream
+            using FileStream fileStreamOutput = File.Create(@"../../../Output.jpg");
+            //Copy the converted image stream into created output stream
+            stream.CopyTo(fileStreamOutput);
+        }
+    }
+}
