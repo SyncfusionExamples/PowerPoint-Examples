5df630d216a16e97b37de8e2a444d459cb2b3206
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2024-01-30 16:17:25 +0530

    Added sample Remove-hidden-slides-in-powerpoint

---
+++ Slides/Remove-hidden-slides-in-powerpoint/.NET/Remove-hidden-slides-in-powerpoint/Program.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Presentation;
+
+namespace Remove_hidden_slides_in_powerpoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Loads or open an PowerPoint Presentation.
+            FileStream inputStream = new FileStream(@"../../../Data/Template.pptx", FileMode.Open);
+            IPresentation pptxDoc = Presentation.Open(inputStream);
+
+            //Remove every hidden slide.
+            RemoveHiddenSlides(pptxDoc);
+
+            //Save the PowerPoint Presentation as stream
+            FileStream outputStream = new FileStream(@"../../../Output.pptx", FileMode.Create);
+            pptxDoc.Save(outputStream);
+            //Release all resources of the stream
+            outputStream.Dispose();
+            //Closes the Presentation instance
+            pptxDoc.Close();
+        }
+
+        /// <summary>
+        /// Removes the hidden slide
+        /// </summary>
+        static void RemoveHiddenSlides(IPresentation pptxDoc)
+        {
+            for (int i = 0; i < pptxDoc.Slides.Count; i++)
+            {
+                if (!pptxDoc.Slides[i].Visible)
+                {
+                    pptxDoc.Slides.Remove(pptxDoc.Slides[i]);
+                    i--;
+                }
+            }
+        }
+    }
+}
