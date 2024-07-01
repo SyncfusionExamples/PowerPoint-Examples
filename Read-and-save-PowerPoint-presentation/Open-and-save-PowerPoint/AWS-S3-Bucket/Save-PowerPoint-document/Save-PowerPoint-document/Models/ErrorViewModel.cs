317791f53661a81e44b45cfaa94cd12d353c51cc
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-01 12:08:59 +0530

    AWS-S3-Bucket

---
+++ Read-and-save-PowerPoint-presentation/Open-and-save-PowerPoint/AWS-S3-Bucket/Save-PowerPoint-document/Save-PowerPoint-document/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Save_PowerPoint_document.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
