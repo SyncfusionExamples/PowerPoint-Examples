592b5d6c598d1cf65f79b48eaf1bd221425d2213
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-08-20 08:28:21 +0530

    Comments

--- Comments/Add-comments-in-PowerPoint/.NET/Add-comments-in-PowerPoint/Program.cs
+++ Comments/Add-comments-in-PowerPoint/.NET/Add-comments-in-PowerPoint/Program.cs
@@ -7,5 +7,5 @@ ISlide slide = pptxDoc.Slides.Add(SlideLayoutType.Blank);
 //Add a comment to the slide.
 slide.Comments.Add(10, 10, "Author1", "A1", "Can we change the font size to 20?", DateTime.Now);
 //Save the PowerPoint Presentation as stream.
-using FileStream outputStream = new(Path.GetFullPath(@"../../../Result.pptx"), FileMode.Create, FileAccess.ReadWrite);
+using FileStream outputStream = new(Path.GetFullPath(@"Output/Output.pptx"), FileMode.Create, FileAccess.ReadWrite);
 pptxDoc.Save(outputStream);
