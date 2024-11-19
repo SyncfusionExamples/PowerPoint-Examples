7e174072b8c67bab4302957e1695a59df0b17269
Suriya-Balamurugan <suriya.balamurugan@syncfusion.com>
2024-11-19 19:35:12 +0530

    Added .NET sample to set PDF conformance level

---
+++ PPTX-to-PDF-conversion/Set-PDF-conformance-level/.NET/Set-PDF-conformance-level/Program.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Presentation;
+using Syncfusion.PresentationRenderer;
+
+//Create a file stream to read the PowerPoint presentation file.
+using FileStream inputStream = new(Path.GetFullPath(@"Data/Template.pptx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+//Open an existing PowerPoint presentation.
+using IPresentation pptxDoc = Presentation.Open(inputStream);
+//Initialize the conversion settings.
+PresentationToPdfConverterSettings pdfConverterSettings = new PresentationToPdfConverterSettings();
+//Set the Pdf conformance level to A1B
+pdfConverterSettings.PdfConformanceLevel = PdfConformanceLevel.Pdf_A1B;
+//Convert the PowerPoint presentation to PDF file.
+using PdfDocument pdfDocument = PresentationToPdfConverter.Convert(pptxDoc, pdfConverterSettings);
+//Create new instance of file stream.
+using FileStream pdfStream = new(Path.GetFullPath(@"Output/PPTXToPDF.pdf"), FileMode.Create);
+//Save the generated PDF to file stream.
+pdfDocument.Save(pdfStream);
