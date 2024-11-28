1c472c6e09f3c9c56616bcd29f0e9bb816bf5c36
Suresh-Maran21 <suresh.sf4645@gmail.com>
2024-11-28 10:23:34 +0530

    Updated GitHub for Ft samples.

---
+++ Charts/Create-simple-pie-chart/.NET/Create-simple-pie-chart/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.OfficeChart;
+using Syncfusion.Presentation;
+
+//Create an instance of presentation.
+using IPresentation presentation = Presentation.Create();
+//Add a blank slide to the presentation.
+ISlide slide = presentation.Slides.Add(SlideLayoutType.Blank);
+//Add a chart to the slide with position and size.
+IPresentationChart chart = slide.Charts.AddChart(100, 10, 700, 500);
+//Select the chart type.
+chart.ChartType = OfficeChartType.Pie;
+//Assign data range.
+chart.DataRange = chart.ChartData[1, 1, 6, 2];
+chart.IsSeriesInRows = false;
+//Set the values for the chart data.
+chart.ChartData.SetValue(1, 1, "Food");
+chart.ChartData.SetValue(2, 1, "Fruits");
+chart.ChartData.SetValue(3, 1, "Vegetables");
+chart.ChartData.SetValue(4, 1, "Dairy");
+chart.ChartData.SetValue(5, 1, "Protein");
+chart.ChartData.SetValue(6, 1, "Grains");
+chart.ChartData.SetValue(1, 2, "Percentage");
+chart.ChartData.SetValue(2, 2, 36);
+chart.ChartData.SetValue(3, 2, 14);
+chart.ChartData.SetValue(4, 2, 13);
+chart.ChartData.SetValue(5, 2, 28);
+chart.ChartData.SetValue(6, 2, 9);
+//Set data labels.
+chart.Series[0].DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
+//Save the presentation.
+using FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.pptx"), FileMode.Create, FileAccess.ReadWrite);
+presentation.Save(outputStream);
---
+++ Paragraphs/Simple-text-and-paragraph-formatting/.NET/Simple-text-and-paragraph-formatting/Program.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Presentation;
+
+//Load PowerPoint Presentation.
+using FileStream inputStream = new(Path.GetFullPath(@"Data/Template.pptx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+//Open an existing PowerPoint presentation.
+using IPresentation pptxDoc = Presentation.Open(inputStream);
+//Retrieve the first slide from Presentation
+ISlide slide = pptxDoc.Slides[0];
+//Retrieve the first shape.
+IShape shape = slide.Shapes[0] as IShape;
+//Retrieve the first paragraph of the shape.
+IParagraph paragraph = shape.TextBody.Paragraphs[0];
+//Set center alignment for the paragraph.
+paragraph.HorizontalAlignment = HorizontalAlignmentType.Center;
+//Retrieve the first TextPart of the shape.
+ITextPart textPart = paragraph.TextParts[0];
+//Set bold for the text.
+textPart.Font.Bold = true;
+//Save the PowerPoint Presentation.
+using FileStream outputStream = new(Path.GetFullPath(@"Output/Result.pptx"), FileMode.Create, FileAccess.ReadWrite);
+pptxDoc.Save(outputStream);
