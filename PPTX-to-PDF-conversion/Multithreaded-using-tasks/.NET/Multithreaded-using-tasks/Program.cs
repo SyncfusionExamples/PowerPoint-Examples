b49a58e4bdd017c2244c70f365ec0d62c2dbcac6
Suriya-Balamurugan <suriya.balamurugan@syncfusion.com>
2024-12-09 20:37:26 +0530

    Add example for multithreading

---
+++ PPTX-to-PDF-conversion/Multithreaded-using-tasks/.NET/Multithreaded-using-tasks/Program.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.Presentation;
+using Syncfusion.Pdf;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Syncfusion.PresentationRenderer;
+
+namespace Multithreaded_using_tasks
+{
+    class MultiThreading
+    {
+        //Indicates the number of threads to be create.
+        private const int TaskCount = 1000;
+        public static async Task Main()
+        {
+            //Create an array of tasks based on the TaskCount.
+            Task[] tasks = new Task[TaskCount];
+            for (int i = 0; i < TaskCount; i++)
+            {
+                tasks[i] = Task.Run(() => ConvertPowerPointToPDF());
+            }
+            //Ensure all tasks complete by waiting on each task.
+            await Task.WhenAll(tasks);
+        }
+
+        //Convert a Powerpoint presentation to PDF using multi-threading.
+        static void ConvertPowerPointToPDF()
+        {
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.pptx"), FileMode.Open, FileAccess.Read))
+            {
+                //Load an existing PowerPoint presentation.
+                using (IPresentation presentation = Presentation.Open(inputStream))
+                {
+                    //Convert PowerPoint presentation to PDF.
+                    using (PdfDocument pdfDocument = PresentationToPdfConverter.Convert(presentation))
+                    {
+                        using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output" + Guid.NewGuid().ToString() + ".pdf"), FileMode.Create, FileAccess.Write))
+                        {
+                            //Save the PDF document.
+                            pdfDocument.Save(outputFileStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Read-and-save-PowerPoint-presentation/Multithreaded-using-tasks/.NET/Multithreaded-using-tasks/Program.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.Presentation;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Multithreaded_using_tasks
+{
+    class MultiThreading
+    {
+        //Indicates the number of threads to be create.
+        private const int TaskCount = 1000;
+        public static async Task Main()
+        {
+            //Create an array of tasks based on the TaskCount.
+            Task[] tasks = new Task[TaskCount];
+            for (int i = 0; i < TaskCount; i++)
+            {
+                tasks[i] = Task.Run(() => OpenAndSavePresentation());
+            }
+            //Ensure all tasks complete by waiting on each task.
+            await Task.WhenAll(tasks);
+        }
+
+        //Open and save a Powerpoint presentation using multi-threading.
+        static void OpenAndSavePresentation()
+        {
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.pptx"), FileMode.Open, FileAccess.Read))
+            {
+                //Load an existing PowerPoint presentation.
+                using (IPresentation presentation = Presentation.Open(inputStream))
+                {
+                    //Add a slide of TitleAndContent type.
+                    ISlide slide = presentation.Slides.Add(SlideLayoutType.TitleAndContent);
+                    //Save the presentation in the desired format.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output" + Guid.NewGuid().ToString() + ".pptx"), FileMode.Create, FileAccess.Write))
+                    {
+                        presentation.Save(outputFileStream);
+                    }
+                }
+            }
+        }
+    }
+}
