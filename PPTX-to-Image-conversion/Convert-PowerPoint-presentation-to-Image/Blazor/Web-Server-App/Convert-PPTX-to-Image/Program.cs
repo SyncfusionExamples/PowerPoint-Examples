9644dc4fda5eb2662961103b7d161436724550c2
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-09-27 17:41:27 +0530

    Added ppt to image Web app server sample

---
+++ PPTX-to-Image-conversion/Convert-PowerPoint-presentation-to-Image/Blazor/Web-Server-App/Convert-PPTX-to-Image/Program.cs
@@ -0,0 +1,29 @@
+using Convert_PowerPoint_Presentation_to_Image.Data;
+using Convert_PPTX_to_Image.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped<PowerPointService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
