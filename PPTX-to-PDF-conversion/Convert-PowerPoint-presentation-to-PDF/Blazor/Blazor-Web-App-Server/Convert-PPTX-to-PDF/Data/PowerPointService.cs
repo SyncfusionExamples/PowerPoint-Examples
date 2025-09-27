6d1c3b195983b25892233062a9d675c4e74506d4
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-09-27 18:03:15 +0530

    Added PPTX to PDF Blazor app server sample

---
+++ PPTX-to-PDF-conversion/Convert-PowerPoint-presentation-to-PDF/Blazor/Blazor-Web-App-Server/Convert-PPTX-to-PDF/Data/PowerPointService.cs
@@ -0,0 +1,33 @@
+using Syncfusion.Presentation;
+using Syncfusion.Pdf;
+using Syncfusion.PresentationRenderer;
+
+namespace Convert_PowerPoint_Presentation_to_PDF.Data
+{
+    public class PowerPointService
+    {
+        public MemoryStream ConvertPPTXtoPDF()
+        {
+            //Open the file as Stream
+            using (FileStream sourceStreamPath = new FileStream(@"wwwroot/Input.pptx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Open the existing PowerPoint presentation with loaded stream.
+                using (IPresentation pptxDoc = Presentation.Open(sourceStreamPath))
+                {
+                    //Convert the PowerPoint document to PDF document.
+                    using (PdfDocument pdfDocument = PresentationToPdfConverter.Convert(pptxDoc))
+                    {
+                        //Create the MemoryStream to save the converted PDF.      
+                        MemoryStream pdfStream = new MemoryStream();
+                        //Save the converted PDF document to MemoryStream.
+                        pdfDocument.Save(pdfStream);
+                        pdfStream.Position = 0;
+
+                        //Download PDF document in the browser.
+                        return pdfStream;
+                    }
+                }
+            }                                           
+        }
+    }
+}
