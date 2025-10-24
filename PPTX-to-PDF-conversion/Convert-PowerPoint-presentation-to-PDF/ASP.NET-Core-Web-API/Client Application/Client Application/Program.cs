4353d35c288b35a5634378038604dc0fe1ef9313
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-10-24 16:20:21 +0530

    Added 3 Web API Samples.

---
+++ PPTX-to-PDF-conversion/Convert-PowerPoint-presentation-to-PDF/ASP.NET-Core-Web-API/Client Application/Client Application/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create an HttpClient instance
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Send a GET request to a URL
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7241/api/Values/api/ConvertToPdf");
+
+                // Check if the response is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the content as a string
+                    Stream responseBody = await response.Content.ReadAsStreamAsync();
+                    FileStream fileStream = File.Create("../../../Output/Output.pdf");
+                    responseBody.CopyTo(fileStream);
+                    fileStream.Close();
+                }
+                else
+                {
+                    Console.WriteLine("HTTP error status code: " + response.StatusCode);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Request exception: " + e.Message);
+            }
+        }
+    }
+}
