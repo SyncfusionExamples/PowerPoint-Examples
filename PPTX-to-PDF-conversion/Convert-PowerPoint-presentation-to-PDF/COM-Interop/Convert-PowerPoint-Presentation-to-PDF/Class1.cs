2ca7e6d53e16ef17135506618a7511100348aa62
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2026-04-21 09:55:51 +0530

    Add Convert-PPTX-to-PDF sample

---
+++ PPTX-to-PDF-conversion/Convert-PowerPoint-presentation-to-PDF/COM-Interop/Convert-PowerPoint-Presentation-to-PDF/Class1.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Presentation;
+using Syncfusion.PresentationToPdfConverter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_PowerPoint_Presentation_to_PDF
+{
+    [ComVisible(true)]
+    [Guid("6F6B4A3C-5C98-4D63-8C7A-5B2C0F1D9E11")] // Use a unique GUID
+    [ProgId("Convert_PowerPoint_Presentation_to_PDF.Class1")] // Stable ProgID for VBScript
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Class1
+    {
+        public void ConvertPowerPointPresentationtoPDF(string inputFilePath, string outputFilePath)
+        {
+            using(IPresentation pptxDoc = Presentation.Open(inputFilePath))
+            {
+                //Converts the PowerPoint Presentation into PDF document
+                using (PdfDocument pdfDocument = PresentationToPdfConverter.Convert(pptxDoc))
+                {
+                    // Save PDF document
+                    pdfDocument.Save(outputFilePath);
+                }
+            }
+        }
+    }
+}
