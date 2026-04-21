f99734eb3aec97adac67d700f8603292fbb94ec9
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2026-04-21 09:46:53 +0530

    Add Open and Save PowerPoint Presentation

---
+++ Read-and-save-PowerPoint-presentation/Open-and-save-PowerPoint/COM-Interop/Open-and-Save-PowerPoint-Presentation/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_and_Save_PowerPoint_Presentation
+{
+    [ComVisible(true)]
+    [Guid("6F6B4A3C-5C98-4D63-8C7A-5B2C0F1D9E11")] // Use a unique GUID
+    [ProgId("Open_and_Save_PowerPoint_Presentation.Class1")] // Stable ProgID for VBScript
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Class1
+    {
+        public void OpenAndSavePowerPointPresentation(string inputFilePath, string outputFilePath)
+        {
+            // Open an existing PowerPoint presentation
+            using (IPresentation pptxDoc = Presentation.Open(inputFilePath))
+            {
+                // Gets the first slide from the PowerPoint presentation
+                ISlide slide = pptxDoc.Slides[0];
+                // Gets the first shape of the slide
+                IShape shape = slide.Shapes[0] as IShape;
+                // Change the text of the shape
+                if (shape.TextBody.Text == "Company History")
+                    shape.TextBody.Text = "Company Profile";
+                // Save the PowerPoint presentation
+                pptxDoc.Save(outputFilePath);
+            }
+            
+        }        
+    }
+}
