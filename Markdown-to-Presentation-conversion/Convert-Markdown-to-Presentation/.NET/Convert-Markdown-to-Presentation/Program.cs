b4bd3572a3a7798c1655d8b9ab806d61e0e49c38
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-05-19 18:36:29 +0530

    Added sample

---
+++ Markdown-to-Presentation-conversion/Convert-Markdown-to-Presentation/.NET/Convert-Markdown-to-Presentation/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Presentation;
+
+namespace Convert_Markdown_to_Presentation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Open the file as a Stream.
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Input.md"), FileMode.Open, FileAccess.Read))
+            {
+                //Load the file stream.
+                using (IPresentation presentation = Presentation.Open(fileStream))
+                {
+                    //Save as a Markdown document into the PPTX FileStream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/MarkdownToPPTX.pptx"), FileMode.Create))
+                    {
+                        presentation.Save(outputStream);
+                    }
+                }
+            }
+        }
+    }
+}
