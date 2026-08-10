3f7cb2f1029fb822578ef413b68ce617541125e9
Viswajith-SF4658 <viswajith.mathialagan@syncfusion.com>
2026-08-10 16:11:47 +0530

    Feedback resolved

--- Read-and-save-PowerPoint-presentation/Edit-PowerPoint-presentation/.NET/Edit-PowerPoint-presentation/Program.cs
+++ Read-and-save-PowerPoint-presentation/Edit-PowerPoint-presentation/.NET/Edit-PowerPoint-presentation/Program.cs
@@ -1,18 +1,19 @@
-﻿//Opens the PPTX document.
-using Syncfusion.Presentation;
+﻿using Syncfusion.Presentation;
 using System.IO;
 
-IPresentation pptxDoc = Presentation.Open(@"Data/Template.pptx");
+//Opens the PPTX document.
+using IPresentation pptxDoc = Presentation.Open(@"Data/Template.pptx");
 
 //Get the new background picture as a stream.
-FileStream bgPictureStream = new FileStream(@"Data/Background.png", FileMode.Open);
+using FileStream bgPictureStream = new FileStream(@"Data/Background.png", FileMode.Open);
 //Create an instance for memory stream
-MemoryStream memoryStream = new MemoryStream();
+using MemoryStream bgMemoryStream = new MemoryStream();
 //Copy stream to memoryStream.
-bgPictureStream.CopyTo(memoryStream);
+bgPictureStream.CopyTo(bgMemoryStream);
+
 //Set the master slide background fill as Picture fill in PPTX document.
 pptxDoc.Masters[0].Background.Fill.FillType = FillType.Picture;
-pptxDoc.Masters[0].Background.Fill.PictureFill.ImageBytes = memoryStream.ToArray();
+pptxDoc.Masters[0].Background.Fill.PictureFill.ImageBytes = bgMemoryStream.ToArray();
 
 //Get the first shape of the first slide from the PPTX document.
 IShape shape = pptxDoc.Slides[0].Shapes[0] as IShape;
@@ -21,13 +22,14 @@ if (shape.TextBody.Text == "Company History")
     shape.TextBody.Text = "Adventure Cycles History";
 
 //Get the new picture as a stream.
-FileStream pictureStream = new FileStream(@"Data/AdventureCycles.png", FileMode.Open);
+using FileStream pictureStream = new FileStream(@"Data/AdventureCycles.png", FileMode.Open);
 //Create an instance for memory stream
-memoryStream = new MemoryStream();
+using MemoryStream picMemoryStream = new MemoryStream();
 //Copy stream to memoryStream.
-pictureStream.CopyTo(memoryStream);
+pictureStream.CopyTo(picMemoryStream);
+
 //Replace the existing image with the new image.
-pptxDoc.Slides[0].Pictures[0].ImageData = memoryStream.ToArray();
+pptxDoc.Slides[0].Pictures[0].ImageData = picMemoryStream.ToArray();
 
 //Changes the built in style of the table.
 ITable table = pptxDoc.Slides[2].Tables[0];
@@ -35,5 +37,3 @@ table.BuiltInStyle = BuiltInTableStyle.ThemedStyle2Accent5;
 
 //Save the PPTX document
 pptxDoc.Save(@"Output/Output.pptx");
-//Closes the PPTX document.
-pptxDoc.Close();
