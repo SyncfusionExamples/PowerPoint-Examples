5cb8ac98369557ba6ca14b94bfb999e70d87b4ae
ATCHAYASEKAR-SF4414 <atchaya.sekar@syncfusion.com>
2026-08-12 12:36:19 +0530

    added the get path

--- PowerPoint-Presentation/Access-built-in-document-properties/.NET/Access-built-in-document-properties/Program.cs
+++ PowerPoint-Presentation/Access-built-in-document-properties/.NET/Access-built-in-document-properties/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Syncfusion.Presentation;
 
 //Open an existing PowerPoint presentation.
-using IPresentation pptxDoc = Presentation.Open(@"../../../Data/Template.pptx");
+using IPresentation pptxDoc = Presentation.Open(Path.GetFullPath(@"../../../Data/Template.pptx"));
 //Accesse the built-in document properties.
 Console.WriteLine("Title - {0}", pptxDoc.BuiltInDocumentProperties.Title);
 Console.WriteLine("Author - {0}", pptxDoc.BuiltInDocumentProperties.Author);
-pptxDoc.Save(@"../../../Result.pptx");
+pptxDoc.Save(Path.GetFullPath(@"../../../Result.pptx"));
